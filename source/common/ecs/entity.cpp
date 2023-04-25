0ede82508a7ed6cf63c6144710b6e6f6fc5d2095
Mohab <hoobamz@gmail.com>
2023-04-25 22:21:30 +0200

    req_8 "Entity cpp"

--- source/common/ecs/entity.cpp
+++ source/common/ecs/entity.cpp
@@ -12,7 +12,14 @@ namespace our {
     // its parent's parent's matrix and so on till you reach the root.
     glm::mat4 Entity::getLocalToWorldMatrix() const {
         //TODO: (Req 8) Write this function
-        return glm::mat4(1.0f);
+        glm::mat4 result = glm::mat4(1.0f);
+        Entity* p = (Entity*)this;
+        while(p->parent != nullptr)
+        {
+            result = localTransform.toMat4() * result;
+            p = p->parent;
+        }
+        return result;
     }
 
     // Deserializes the entity data and components from a json object
