0d84e48ec0e5df0eed8feff8b64c14526119973f
Mohab <hoobamz@gmail.com>
2023-04-25 22:47:38 +0200

    req_8 "World"

--- source/common/ecs/world.hpp
+++ source/common/ecs/world.hpp
@@ -26,7 +26,10 @@ namespace our {
         Entity* add() {
             //TODO: (Req 8) Create a new entity, set its world member variable to this,
             // and don't forget to insert it in the suitable container.
-            return nullptr;
+            Entity* ptr = new Entity();
+            ptr->world = (World*)this;
+            entities.insert(ptr);
+            return ptr;
         }
 
         // This returns and immutable reference to the set of all entites in the world.
@@ -38,17 +41,37 @@ namespace our {
         // The elements in the "markedForRemoval" set will be removed and deleted when "deleteMarkedEntities" is called.
         void markForRemoval(Entity* entity){
             //TODO: (Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
+            for(auto it = entities.begin(); it != entities.end(); it++)
+                if(*it == entity)
+                {
+                    markedForRemoval.insert(entity);
+                    return;
+                }
         }
 
         // This removes the elements in "markedForRemoval" from the "entities" set.
         // Then each of these elements are deleted.
         void deleteMarkedEntities(){
             //TODO: (Req 8) Remove and delete all the entities that have been marked for removal
+            while(!markedForRemoval.empty())
+            {
+                auto it = markedForRemoval.begin();
+                entities.erase(*it);
+                delete *it;
+                markedForRemoval.erase(*it);
+            }
         }
 
         //This deletes all entities in the world
         void clear(){
             //TODO: (Req 8) Delete all the entites and make sure that the containers are empty
+            deleteMarkedEntities();
+            while(!entities.empty())
+            {
+                auto it = entities.begin();
+                entities.erase(*it);
+                delete *it;
+            }
         }
 
         //Since the world owns all of its entities, they should be deleted alongside it.
