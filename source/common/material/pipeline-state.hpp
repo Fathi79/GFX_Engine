b722356b6e68571eb20e60939edb61c658f3d4c1
Mohab <hoobamz@gmail.com>
2023-04-25 20:58:43 +0200

    req-4 done

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -42,6 +42,41 @@ namespace our {
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
             //TODO: (Req 4) Write this function
+            //Specifing whether the individual color components in the frame buffer can be written. It affects all thedrawin buffers
+            glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
+            //Controls writing access in the depth buffer
+            glDepthMask(depthMask);
+            //Here we are setting the faceculling options using the values in the struct instance 'faceCulling'
+            if(faceCulling.enabled)
+            {
+                glEnable(GL_CULL_FACE);
+                glCullFace(faceCulling.culledFace);
+                glFrontFace(faceCulling.frontFace);
+            }
+            else glDisable(GL_CULL_FACE);
+
+            //Here we are setting the blending options using the values in the struct instance 'blending'
+            if(blending.enabled)    //Check if we want to enable blending options
+            {
+                glEnable(GL_BLEND);
+                glBlendEquation(blending.equation);     //Setting the blending equation to be used
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);     //Setting the source and destination factors. exampleEq: srcColor*srcFactor + destColor*destFactor
+                // srcColor:  color output of the fragment shader
+                // destColor: color vector currently stored in the color buffer
+                // srcFactor: sets the impact of the alpha value on the source color
+                // destFactor: sets the impact of the alpha value on the destination color
+                //Note: These factors are meaningless if the blending equation is GL_MIN or GL_MAX
+                glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);   //Setting the blendColor
+            }
+            else glDisable(GL_BLEND);
+
+            //Here we are setting the depth testing options using the values in the struct instance 'depthTesting'
+            if(depthTesting.enabled)
+            {
+                glEnable(GL_DEPTH_TEST);
+                glDepthFunc(depthTesting.function);    //Setting the comparator operator used by openGL. To control when OpenGL should pass or discard fragments and when to update depth buffer
+            }
+            else glDisable(GL_DEPTH_TEST);
         }
 
         // Given a json object, this function deserializes a PipelineState structure
