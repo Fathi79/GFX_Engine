1a23a953f59655dade8d89a3113b62cb8288ea7e
Abdelrhman M.Fathy <fathi791112@gmail.com>
2023-04-27 00:50:17 +0200

    Req-7

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -8,6 +8,9 @@ namespace our {
     // This function should setup the pipeline state and set the shader to be used
     void Material::setup() const {
         //TODO: (Req 7) Write this function
+        pipelineState.setup();//pipline setup call 
+        shader->use();//use the progam shader
+
     }
 
     // This function read the material data from a json object
@@ -25,6 +28,8 @@ namespace our {
     // set the "tint" uniform to the value in the member variable tint 
     void TintedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+        Material::setup();//parent call
+        shader->set("tint",tint);///set the uniform location of the vect4 tint
     }
 
     // This function read the material data from a json object
@@ -39,6 +44,13 @@ namespace our {
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
     void TexturedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+        TintedMaterial::setup();
+        shader->set("alphaThreshold",alphaThreshold);   
+       // glActiveTexture(GL_TEXTURE0); // we select an active texture unit. 
+        texture->bind();
+        sampler->bind(0);
+        shader->set("tex",0);
+
     }
 
     // This function read the material data from a json object
